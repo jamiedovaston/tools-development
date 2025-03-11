4130e4dbf5a2094ccbe9a0d47da66ee385668812
Jamie Dovaston <jamiedovaston@outlook.com>
2025-03-11 14:08:03 +0000

    changes in session

---
+++ tools-development-unity-workspace/Assets/JD Look Outside Asset/Scripts/Models/Weather.cs
@@ -0,0 +1,10 @@
+﻿namespace JD.Models
+{
+    public class Weather
+    {
+        public int id;
+        public string main;
+        public string description;
+        public string icon;
+    }
+}
